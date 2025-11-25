a2f078fccfffa509856e26d4e648475ee1a61a47
KyAnhVo <tommyvo0406@gmail.com>
2025-11-25 23:20:57 +0000

    initialize target network in DeepQStrat

--- agents/deepq_strat.py
+++ agents/deepq_strat.py
@@ -1,7 +1,12 @@
 from agents.state_translator import STRAT_STATE_DIM_COUNT
 from agents.deep_uno_agent import DeepUnoAgent
+from agents.deeprl_nn import DeepRL_NN
 
 class DeepQStratAgent(DeepUnoAgent):
+    target_nn: DeepRL_NN
     def __init__(self):
         super().__init__(state_dim=STRAT_STATE_DIM_COUNT)
+        self.target_nn = DeepRL_NN(state_dim=STRAT_STATE_DIM_COUNT,
+                                   action_dim=61)
+    
 
