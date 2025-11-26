fa60bc2f1175f9cc19f6afaab78d5c8fb64bdf96
KyAnhVo <tommyvo0406@gmail.com>
2025-11-26 00:31:48 +0000

    deepqstratagent override state translation and compute_targets

--- agents/deepq_strat.py
+++ agents/deepq_strat.py
@@ -1,12 +1,33 @@
-from agents.state_translator import STRAT_STATE_DIM_COUNT
+from agents.state_translator import STRAT_STATE_DIM_COUNT, strategic_state_translate
 from agents.deep_uno_agent import DeepUnoAgent
 from agents.deeprl_nn import DeepRL_NN
 
+from typing import override, List
+import torch
+
 class DeepQStratAgent(DeepUnoAgent):
     target_nn: DeepRL_NN
+    episodes: int
+
     def __init__(self):
         super().__init__(state_dim=STRAT_STATE_DIM_COUNT)
         self.target_nn = DeepRL_NN(state_dim=STRAT_STATE_DIM_COUNT,
                                    action_dim=61)
+        self.target_nn.load_state_dict(self.online_nn.state_dict())
     
+    @override
+    def state_translation(self, state) -> List[int]:
+        return strategic_state_translate(state)
+
+    @override
+    def compute_targets(self) -> List[float]:
+        next_q_values = self.target_nn.forward(
+                torch.tensor(self.next_state_list, dtype=torch.float32))
+        max_next_q = next_q_values.max(dim=1).values
+        rewards = torch.tensor(self.rewards_list, dtype=torch.float32)
+        dones = torch.tensor(self.dones, dtype=torch.float32)
+
+        return (rewards + self.gamma * max_next_q * (1 - dones)).tolist()
+
+
 
